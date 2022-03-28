b62618e0283c171978abc68bdd53f6f2b80fc9bd
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-03-28 12:35:19 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(num1, num2) {
+    return 2000 / 100;
+}
+
+
+const square = (parameter1) => parameter1 ** parameter1;
+
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
